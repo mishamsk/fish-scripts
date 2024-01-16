b76558d33f58e3c1bf0299eac9ead27c6ae573b9
mishamsk <mishamsk@gmail.com>
2024-01-15 19:39:10 -0500

    ✨ py_create_package will create a whole hierarchy of packages now ✨ do not reload auto-loading functions in reload-functions

--- fish/reload-functions.fish
+++ fish/reload-functions.fish
@@ -1,5 +1,9 @@
 function reload-functions
     for func in $__fish_config_dir/functions/*.fish
+        if string match -q $func $__fish_config_dir/functions/_*.fish
+            # Skip autoload functions
+            continue
+        end
         source $func
     end
 end
--- python/py_create_package.fish
+++ python/py_create_package.fish
@@ -1,10 +1,28 @@
 function py_create_package --description 'Create an empty Python package folder with empty __init__.py'
-    if test ! -e $argv
-        mkdir $argv
-        touch $argv/__init__.py
-    else
-        if test -d $argv -a ! -e $argv/__init__.py
-            touch $argv/__init__.py
+
+    if string match -q /* $argv
+        echo "Error: Path must be relative"
+        return 1
+    end
+
+    set -l dirs (string split / $argv)
+
+    if test (count $dirs) -lt 1
+        echo "Error: Path must contain at least one folder"
+        return 1
+    end
+
+    set -l dir ''
+
+    for cmp in $dirs
+        set -l dir $dir$cmp
+        echo "Checking & creating package at $dir"
+
+        if test ! -e $dir
+            mkdir $dir
+            touch $dir/__init__.py
         end
+
+        set -l dir $dir/
     end
 end
